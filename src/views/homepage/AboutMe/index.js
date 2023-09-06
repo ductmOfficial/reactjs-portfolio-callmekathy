83c75804cabe4c35db0e48c836ab5ae118825ffd
D O U G L A S <ductm.official@gmail.com>
2023-09-06 15:22:19 +0700

    style: apply transition

--- src/views/homepage/AboutMe/index.js
+++ src/views/homepage/AboutMe/index.js
@@ -3,7 +3,6 @@ import { useEffect, useRef } from 'react';
 // material-ui
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
@@ -34,83 +33,90 @@ const AboutMe = () => {
   return (
     <Box ref={revealContainer} component="section" id="about-me" sx={{ py: 8, bgcolor: 'background.default' }}>
       <Container maxWidth="xl">
-        <Box maxWidth={1000} margin="0 auto">
-          <Box display="flex" gap={{ xs: 4, lg: 6 }} flexDirection={{ xs: 'column', lg: 'row' }}>
-            <Pic>
-              <div className="wrapper">
-                <img src={ImageAvatar} alt="Avatar" className="img" />
-              </div>
-            </Pic>
-            <Box flex={1}>
-              <Typography variant="numberedHeading" component="h2" gutterBottom>
-                About me
-              </Typography>
-              <Typography gutterBottom>
-                Hello! My name is Brittany and I enjoy creating things that live on the internet. My interest in web development started
-                back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught
-                me a lot about HTML & CSS!
-              </Typography>
-              <Typography gutterBottom>
-                Fast-forward to today, and I’ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and a
-                student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at
-                Upstatement for a variety of clients.
-              </Typography>
-              <Typography gutterBottom>
-                I also recently launched a course that covers everything you need to build a web app with the Spotify API using Node &
-                React.
-              </Typography>
-
-              <Divider sx={{ my: 2 }} />
-
-              <Grid container spacing={2}>
-                <Grid item xs={12} lg={6}>
-                  <Box>
-                    <Typography variant="h5">Phone</Typography>
-                    <Typography color="text.secondary">{profile.phoneNumber}</Typography>
-                  </Box>
+        <Box maxWidth={1200} margin="0 auto">
+          <Grid container spacing={{ xs: 2, sm: 4 }}>
+            <Grid item xs={12} sm={12} lg={4}>
+              <Box sx={{ display: 'flex', justifyContent: { xs: 'center', lg: 'flex-start' }, mb: { xs: 4, lg: 0 } }}>
+                <Pic>
+                  <div className="wrapper">
+                    <img src={ImageAvatar} alt="Avatar" className="img" />
+                  </div>
+                </Pic>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={6} lg={4}>
+              <Box sx={{ textAlign: { sm: 'right' } }}>
+                <Typography variant="numberedHeading" component="h2" gutterBottom>
+                  About me
+                </Typography>
+                <Typography gutterBottom>
+                  Hello! My name is Brittany and I enjoy creating things that live on the internet. My interest in web development started
+                  back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught
+                  me a lot about HTML & CSS!
+                </Typography>
+                <Typography gutterBottom>
+                  Fast-forward to today, and I’ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and
+                  a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences
+                  at Upstatement for a variety of clients.
+                </Typography>
+                <Typography gutterBottom>
+                  I also recently launched a course that covers everything you need to build a web app with the Spotify API using Node &
+                  React.
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={6} lg={4}>
+              <Box>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Box>
+                      <Typography variant="h5">Phone</Typography>
+                      <Typography color="text.secondary">{profile.phoneNumber}</Typography>
+                    </Box>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Box>
+                      <Typography variant="h5">Email</Typography>
+                      <Typography color="text.secondary">{profile.email}</Typography>
+                    </Box>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Box>
+                      <Typography variant="h5">Location</Typography>
+                      <Typography color="text.secondary">{`${profile.address.city}, ${profile.address.country}`}</Typography>
+                    </Box>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Box>
+                      <Typography variant="h5">Skype</Typography>
+                      <Typography color="text.secondary">{profile.social.skype}</Typography>
+                    </Box>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Box>
+                      <Typography variant="h5" gutterBottom>
+                        You can follow me also here:
+                      </Typography>
+                      <Stack direction="row" spacing={1}>
+                        <IconButton color="primary" href={profile.social.facebook} target="_blank" sx={{ bgcolor: 'background.paper' }}>
+                          <IconBrandFacebook stroke={1.5} size="1.3rem" />
+                        </IconButton>
+                        <IconButton color="primary" href={profile.social.linkedin} target="_blank" sx={{ bgcolor: 'background.paper' }}>
+                          <IconBrandLinkedin stroke={1.5} size="1.3rem" />
+                        </IconButton>
+                        <IconButton color="primary" href={profile.social.instagram} target="_blank" sx={{ bgcolor: 'background.paper' }}>
+                          <IconBrandInstagram stroke={1.5} size="1.3rem" />
+                        </IconButton>
+                        <IconButton color="primary" href={`skype:${profile.social.skype}?chat`} sx={{ bgcolor: 'background.paper' }}>
+                          <IconBrandSkype stroke={1.5} size="1.3rem" />
+                        </IconButton>
+                      </Stack>
+                    </Box>
+                  </Grid>
                 </Grid>
-                <Grid item xs={12} lg={6}>
-                  <Box>
-                    <Typography variant="h5">Email</Typography>
-                    <Typography color="text.secondary">{profile.email}</Typography>
-                  </Box>
-                </Grid>
-                <Grid item xs={12} lg={6}>
-                  <Box>
-                    <Typography variant="h5">Location</Typography>
-                    <Typography color="text.secondary">{`${profile.address.city}, ${profile.address.country}`}</Typography>
-                  </Box>
-                </Grid>
-                <Grid item xs={12} lg={6}>
-                  <Box>
-                    <Typography variant="h5">Skype</Typography>
-                    <Typography color="text.secondary">{profile.social.skype}</Typography>
-                  </Box>
-                </Grid>
-                <Grid item xs={12} lg={6}>
-                  <Box>
-                    <Typography variant="h5" gutterBottom>
-                      You can follow me also here:
-                    </Typography>
-                    <Stack direction="row" spacing={1}>
-                      <IconButton color="primary" href={profile.social.facebook} target="_blank" sx={{ bgcolor: 'background.paper' }}>
-                        <IconBrandFacebook stroke={1.5} size="1.3rem" />
-                      </IconButton>
-                      <IconButton color="primary" href={profile.social.linkedin} target="_blank" sx={{ bgcolor: 'background.paper' }}>
-                        <IconBrandLinkedin stroke={1.5} size="1.3rem" />
-                      </IconButton>
-                      <IconButton color="primary" href={profile.social.instagram} target="_blank" sx={{ bgcolor: 'background.paper' }}>
-                        <IconBrandInstagram stroke={1.5} size="1.3rem" />
-                      </IconButton>
-                      <IconButton color="primary" href={`skype:${profile.social.skype}?chat`} sx={{ bgcolor: 'background.paper' }}>
-                        <IconBrandSkype stroke={1.5} size="1.3rem" />
-                      </IconButton>
-                    </Stack>
-                  </Box>
-                </Grid>
-              </Grid>
-            </Box>
-          </Box>
+              </Box>
+            </Grid>
+          </Grid>
         </Box>
       </Container>
     </Box>
